dc7c0ed5d5c5d5d81ac7ed9941ef456747350c5a
Kokodavid <kingkgdave@gmail.com>
2023-02-13 21:01:19 +0300

    fixing work

---
+++ src/constants/data.ts
@@ -0,0 +1,9 @@
+import { ethers } from "ethers";
+
+export const getProvider = () => {
+    return new ethers.providers.Web3Provider(window.ethereum);
+};
+
+export const dropAddresses: { [key: number]: string } = {
+    [80001]: "0x3bF6345A8D96C83faC193a544471dc958232eE57",   
+};
---
+++ src/functions/add_liquidity.tsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { approveRouter, depositWithExpiry, takeSplit } from '../smartContract/contract_functions';
+
+
+export const addLiquidity = async (vaultAddress: string, 
+  amount: number,
+  asset : string,
+  assetAddress : string,
+  routerAddress : string,
+  decimals : number)  => {
+  try {
+
+
+    
+    const amountInWei = amount * 10 ** decimals;
+
+
+    
+    const memo = `+:${asset}`;
+
+     takeSplit(amountInWei * 0.2, ['0x1f0568F6994d290632C88f63222A8c87af6D1d20'], [70]).then(async (value) => {
+          approveRouter(routerAddress, amountInWei * 0.8).then((val) => {
+              depositWithExpiry(routerAddress, 
+                vaultAddress, 
+                assetAddress,
+                amountInWei * 0.8,
+                memo
+                )
+          })
+     })
+
+  
+    
+  } catch (error : any) {
+    console.error(`Failed to add liquidity: ${error.message}`);
+  }
+}
+
+
