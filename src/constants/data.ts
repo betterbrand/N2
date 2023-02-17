18a5470e61cbca125bfd18e2a65b9a8f8eae3887
theKurrent <scott@kurrent.dev>
2023-02-17 12:35:06 -0800

    updated for split transaction

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -5,11 +5,6 @@ export const getProvider = () => {
 };
 
 export const dropAddresses: { [key: number]: string } = {
-<<<<<<< HEAD
-    [80001]: "0x3bF6345A8D96C83faC193a544471dc958232eE57",
-    [5]: "OxEecc5bFd3C7d1510Fe6c163008Dd22c33C07A23A"   
-=======
     [80001]: "0x3bF6345A8D96C83faC193a544471dc958232eE57",  
     [5]: "0x32d0BD10fCeD76769ca01aBa8Da930C93f937F1E"
->>>>>>> 0baa39f1345f27842f1882b66f328d3fc66da246
 };
