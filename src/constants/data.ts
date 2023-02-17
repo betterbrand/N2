a404f7bd9f54e018ac867b175d2e29f7e7be7af0
theKurrent <scott@kurrent.dev>
2023-02-17 11:36:50 -0800

    basic UI fixes

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -5,5 +5,6 @@ export const getProvider = () => {
 };
 
 export const dropAddresses: { [key: number]: string } = {
-    [80001]: "0x3bF6345A8D96C83faC193a544471dc958232eE57",   
+    [80001]: "0x3bF6345A8D96C83faC193a544471dc958232eE57",
+    [5]: "OxEecc5bFd3C7d1510Fe6c163008Dd22c33C07A23A"   
 };
