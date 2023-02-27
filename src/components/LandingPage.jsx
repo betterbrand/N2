f44956775fe4cfc7d97a62f512032249d4da5ba7
theKurrent <scott@kurrent.dev>
2023-02-26 16:24:27 -0800

    calculated fees

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -131,12 +131,12 @@ const LandingPage = () => {
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Stake for Freedom</h2>
-                <p className="mt-2 text-center text-sm text-gray-600">
+                {/* <p className="mt-2 text-center text-sm text-gray-600">
                     Or{' '}
                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
                         learn more about our service
                     </a>
-                </p>
+                </p> */}
             </div>
 
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
