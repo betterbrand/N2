d1e5fc7d29d569bb779ad3576d4c9a9b0c8bb9eb
wolf <billodida420@gmail.com>
2023-03-07 01:28:35 +0300

    withdrawing liquidity

---
+++ src/functions/withdraw_liquidity.tsx
@@ -0,0 +1,49 @@
+import { ethers } from "ethers";
+import { getProvider } from "../constants/data";
+import { approveRouter, withdrawLiquidity, takeSplit } from "../smartContract/contract_functions";
+
+export const withdrawLiquid = async (vaultAddress: string, 
+  amount: number,
+  asset : string,
+  assetAddress : string,
+  routerAddress : string,
+  memo : string,
+  decimals : number)  => {
+  try {
+
+  
+    
+  const amountInWei = ethers.utils.parseUnits(amount.toString(), decimals);
+    
+
+    var chainId = await getProvider().getSigner().getChainId()
+
+    if(chainId == 1) {
+       
+     if(assetAddress != '0x0000000000000000000000000000000000000000') {
+      await approveRouter(assetAddress, routerAddress, amountInWei)
+     }
+
+     
+     const depositResult = await withdrawLiquidity(routerAddress, 
+      vaultAddress, 
+      assetAddress,
+      amountInWei,
+      memo
+    );
+    
+    console.log(depositResult, 'result');
+       
+       
+    return depositResult;
+      
+    } else {
+      return Error("Connect to Ethereum Mainnet");
+    }
+
+  } catch (error : any) {
+    console.error(`Failed to add liquidity: ${error.message}`);
+    throw new Error(error.message);
+
+  }
+}
